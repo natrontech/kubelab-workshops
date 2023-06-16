e362daf3b5f07c6431bf85d8f8e59703a3c2938e
Jan Fuhrer <janfuhrer@mailbox.org>
2023-06-16 16:38:33 +0200

    chore: small improvements, update structure

--- kubernetes-basics/01_introduction/01_get_kubectl_version/check.sh
+++ kubernetes-basics/01_introduction/01_get_kubectl_version/check.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Check if the amount of kubernetes nodes is equal to the number in clusterinfo.txt
+# Check if the kubectl version equal to the number in kubectl_client_version.txt
 # If it is, exitcode 0, else exitcode 1
 
 client_version=$(kubectl version  --short | awk '/Client Version: / { print $NF }')
 
-# Check if clusterinfo.txt exists
+# Check if kubectl_client_version.txt exists
 if [ ! -f ~/kubectl_client_version.txt ]; then
   echo "kubectl_client_version.txt does not exist"
   exit 1
 fi
 
+# Check submitted version
 submitted=$(cat ~/kubectl_client_version.txt)
 
 if [ $client_version == $submitted ]; then
--- kubernetes-basics/02_first_steps/02_edit_pod/hint.md
+++ kubernetes-basics/02_first_steps/02_edit_pod/hint.md
@@ -1,7 +1,8 @@
 **hint**: You can use the `kubectl label` command to edit a pod. You can find the documentation [here](https://kubernetes.io/docs/reference/kubectl/#operations).
 
 You can also use the `kubectl edit` command to edit a pod.
-Set the environment variable `KUBE_EDITOR` to `nano` to use the nano editor instread of vim.
-```
+Set the environment variable `KUBE_EDITOR` to `nano` to use the nano editor instead of vim.
+
+```bash
 export KUBE_EDITOR=nano
 ```
