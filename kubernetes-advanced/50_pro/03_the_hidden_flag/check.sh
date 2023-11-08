969ad592f345db561c10b07672aaf9388b8d82d1
Jan Lauber <jan.lauber@protonmail.ch>
2023-11-08 14:09:54 +0100

    fix: check path
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- kubernetes-advanced/50_pro/03_the_hidden_flag/check.sh
+++ kubernetes-advanced/50_pro/03_the_hidden_flag/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Check if the content ~/exercise/exercise/flag.txt is K8S-ENIGMA-FLAG-67890
-flag=$(cat ~/exercise/exercise/flag.txt)
+# Check if the content ~/exercise/flag.txt is K8S-ENIGMA-FLAG-67890
+flag=$(cat ~/exercise/flag.txt)
 
 if [[ "$flag" != "K8S-ENIGMA-FLAG-67890" ]]; then
     echo "The flag is incorrect. Please try again."
